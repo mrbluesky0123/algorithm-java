9a48509876dff16bc999ef4224c7057007097df8
mrbluesky <gogetit88@gmail.com>
2021-03-16 18:08:07 +0900

    WalkerHeaven

--- src/me/mrbluesky/algorithm/walkerheaven/WalkerHeaven.java
+++ src/me/mrbluesky/algorithm/walkerheaven/WalkerHeaven.java
@@ -1,14 +1,53 @@
 package me.mrbluesky.algorithm.walkerheaven;
 
 public class WalkerHeaven {
+
     private int MOD = 20170805;
-    private int[][] map;
+    private int[][] right;
+    private int[][] down;
+    private int r;
+    private int c;
+    private int map[][];
     public int solution(int m, int n, int[][] cityMap) {
         int answer = 0;
-        return answer;
+        right = new int[m+1][n+1];
+        down = new int[m+1][n+1];
+        r = m;
+        c = n;
+        map = cityMap;
+        right[1][1] = down[1][1] = 1;
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(cityMap[i-1][j-1] == 0) {
+                    right[i][j] += (right[i][j-1] + down[i-1][j]) % MOD;
+                    down[i][j] += (right[i][j-1] + down[i-1][j]) % MOD;
+                } else if(cityMap[i-1][j-1] == 2) {
+                    right[i][j] = right[i][j-1];
+                    down[i][j] = down[i-1][j];
+                }
+                showProcess();
+            }
+        }
+        return (right[m][n-1] + down[m-1][n]) % MOD;
     }
 
-    public int turnRight()
+    public void showProcess() {
+        System.out.println("---------------------------");
+        System.out.println("<RIGHT>");
+        for(int i = 0; i <= r; i++) {
+            for(int j = 0; j <= c; j++) {
+                System.out.print(" " + right[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("<DOWN>");
+        for(int i = 0; i <= r; i++) {
+            for(int j = 0; j <= c; j++) {
+                System.out.print(" " + down[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
 }
 
--- src/me/mrbluesky/algorithm/walkerheaven/WalkerHeavenMain.java
+++ src/me/mrbluesky/algorithm/walkerheaven/WalkerHeavenMain.java
@@ -1,4 +1,9 @@
 package me.mrbluesky.algorithm.walkerheaven;
 
 public class WalkerHeavenMain {
+    public static void main(String[] args) {
+        WalkerHeaven wh = new WalkerHeaven();
+        int[][] a = new int[][]{{0, 2, 0, 0, 0, 2}, {0, 0, 2, 0, 1, 0}, {1, 0, 0, 2, 2, 0}};
+        System.out.println(wh.solution(3, 6, a));
+    }
 }
